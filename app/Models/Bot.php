4d876253a6cd2ea43d264f1770d6da5a791d4cb2
Tony Messias <tonysm@hey.com>
2024-01-02 22:45:31 -0300

    wip

--- app/Models/Bot.php
+++ app/Models/Bot.php
@@ -11,7 +11,7 @@ class Bot
         $callback ??= fn() => null;
 
         $answers = [
-            'You first intercept the `turbo:submit-start` event and add a custom `text/vnd.chunked-turbo-stream.html` content type to the `Accept` header. Then, we can detect that in the backend, hold the connection by returning a chunked response using the `Transfer-Encoding: chunked` header, and stream many Turbo Streams down the wire. Finally, in the frontend, we can intercept the `turbo:before-fetch-response` event, check if the response has the custom content type of `text/vnd.chunked-turbo-stream.html`, prevent the default behavior of Turbo, and start reading the chunks and render the Turbo Streams.',
+            'You first intercept the `turbo:submit-start` event and add a custom `text/vnd.streamed-turbo-stream.html` content type to the `Accept` header. Then, we can detect that in the backend, hold the connection and start streaming Turbo Streams down the wire. Finally, in the frontend, we can intercept the `turbo:before-fetch-response` event, check if the response has the custom content type of `text/vnd.streamed-turbo-stream.html`, prevent the default behavior of Turbo, start reading the streamed chunks and render them as Turbo Streams.',
             'You\'re welcome!',
         ];
 
